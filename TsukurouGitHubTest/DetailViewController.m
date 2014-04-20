d1a9a829c22810557c5f05cc336d34c883ba79fd
Nobuki Yura <nobuki.yura@gmail.com>
2014-04-20 16:28:12 +0900

    Yura_test
    20140420

--- TsukurouGitHubTest/DetailViewController.m
+++ TsukurouGitHubTest/DetailViewController.m
@@ -41,7 +41,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
     [self configureView];
     
-    NSLog(@"test");
+//    NSLog(@"test");
 }
 
 - (void)didReceiveMemoryWarning
